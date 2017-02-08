fe1b3bcdb1542be6b03f3f936f296a8889b06145
pbabalis2 <pbabalis@msn.com>
2017-02-08 15:09:17 -0800

    Update app.rb

--- app.rb
+++ app.rb
@@ -9,10 +9,10 @@ get "/" do
   erb :home
 end
  
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
+end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
