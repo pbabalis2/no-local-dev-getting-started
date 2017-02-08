ce1ce57e3eafd44428b50cb025552005a89de794
pbabalis2 <pbabalis@msn.com>
2017-02-08 12:12:20 -0800

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,25 +3,20 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
- 
-
+  
 class Contact < ActiveRecord::Base
-  self.table_name = 'salesforce.contact'
+  self.table_name = 'salesforcejoek.contact'
 end
 
 get "/contacts" do
   @contacts = Contact.all
   erb :index
 end
+
 get "/" do
   erb :home
 end
-
-
-class Contact < ActiveRecord::Base
-  self.table_name = 'salesforce.contact'
-end
-
+ 
 #get "/contacts" do
 #  @contacts = Contact.all
 #  erb :index
